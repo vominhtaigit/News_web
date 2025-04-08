88aa47151568f575a418468f3d1cf848f91d584c
Vo Minh Tai <vominhtai@gmail.com>
2025-04-09 01:39:20 +0700

    Update code

--- src/models/newsModel.js
+++ src/models/newsModel.js
@@ -5,7 +5,7 @@ const newsSchema = new mongoose.Schema({
     content: { type: String, required: true },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }, // Không bắt buộc
-
+    publishedDate: { type: Date, default: Date.now },
     image: { type: String, required: false }, // Thêm trường ảnh
 
     createdAt: { type: Date, default: Date.now },
